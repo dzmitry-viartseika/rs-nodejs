58f0800e3a4a3f4aadd326a57371ba8c17030ef2
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-06-17 11:55:46 +0200

    task #1 - implement copy method according to requirements - implement utils functions for task - add constant errorMessage

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,21 +1,21 @@
 import fs from 'fs/promises';
 import { getDirectoryNamePath } from './utils/getDirectoryNamePath.js';
 import { fileNameURLToPath } from './utils/fileNameURLToPath.js';
+import { ERROR_MESSAGE } from './constants/errorMessage.js';
 
 
 const filename = fileNameURLToPath(import.meta.url);
 const dirname = getDirectoryNamePath(filename);
 
 const newText = 'I am fresh and young';
-const errorMessage = 'FS operation failed';
 export const create = async () => {
     const srcPath = `${dirname}/files/fresh.text`;
 
     try {
         await fs.writeFile(srcPath, newText, { flag: 'wx'});
         console.log(`${newText} have added to the fresh.txt`);
-    } catch (e) {
-        throw new Error(errorMessage);
+    } catch (err) {
+        throw new Error(ERROR_MESSAGE);
     }
 };
 
