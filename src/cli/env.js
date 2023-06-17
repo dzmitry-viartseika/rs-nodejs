742370bb218597d0cc78a260f0f360f608887414
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-06-17 13:28:39 +0200

    task #1 - implement cli env method according to requirements - add constant keyRSS

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+import { KEY_RSS } from './constants/keyRSS.js';
+
 const parseEnv = () => {
-    // Write your code here 
+    const rssVariables = Object.entries(process.env).reduce((acc, [key, value]) => {
+        if (key.startsWith(KEY_RSS)) {
+            acc.push(`${key}=${value}`);
+        }
+
+        return acc;
+    }, []);
+
+    console.log('rssVariables', rssVariables.join('; '))
 };
 
 parseEnv();
