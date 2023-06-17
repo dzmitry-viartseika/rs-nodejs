38b9212345ade87b766723f8961c70acb7d48930
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-06-17 23:45:04 +0200

    task #1 - implement write method according to requirements

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'fs';
+import { fileNameURLToPath } from '../fs/utils/fileNameURLToPath.js';
+import { getDirectoryNamePath } from '../fs/utils/getDirectoryNamePath.js';
+import {UTF_FORMAT} from "../fs/constants/utf8.js";
+const filename = fileNameURLToPath(import.meta.url);
+const dirname = getDirectoryNamePath(filename);
 const read = async () => {
-    // Write your code here 
+    const file = `${dirname}/files/fileToRead.txt`;
+    const readStream = createReadStream(file, UTF_FORMAT);
+    readStream.on('data', (data) => {
+        process.stdout.write(data);
+    })
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from 'fs';
+import { fileNameURLToPath } from '../fs/utils/fileNameURLToPath.js';
+import { getDirectoryNamePath } from '../fs/utils/getDirectoryNamePath.js';
+const filename = fileNameURLToPath(import.meta.url);
+const dirname = getDirectoryNamePath(filename);
 const write = async () => {
-    // Write your code here 
+    const file = `${dirname}/files/fileToRead.txt`;
+    const writeStream = createWriteStream(file);
+    process.stdout.on('data', (data) => {
+        writeStream.write(data.toString());
+    })
+    console.log('check you console')
 };
 
 await write();
