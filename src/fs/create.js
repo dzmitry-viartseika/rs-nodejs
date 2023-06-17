19336e4a34b8b99ddfdf6b5ac82f76b2c9d08b33
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-06-17 11:27:45 +0200

    task #1 - implement create method according to requirements

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
-const create = async () => {
-    // Write your code here 
+import fs from 'fs/promises';
+import { getDirectoryNamePath } from './utils/getDirectoryNamePath.js';
+import { fileNameURLToPath } from './utils/fileNameURLToPath.js';
+
+
+const filename = fileNameURLToPath(import.meta.url);
+const dirname = getDirectoryNamePath(filename);
+
+const newText = 'I am fresh and young';
+const errorMessage = 'FS operation failed';
+export const create = async () => {
+    const srcPath = `${dirname}/files/fresh.text`;
+
+    try {
+        await fs.writeFile(srcPath, newText, { flag: 'wx'});
+        console.log(`${newText} have added to the fresh.txt`);
+    } catch (e) {
+        throw new Error(errorMessage);
+    }
 };
 
 await create();
