f785da5b7745900e56c5a65b35af0a0e1f6b8385
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-06-18 00:14:17 +0200

    task #1 - implement compress method according to requirements

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { fileNameURLToPath } from '../fs/utils/fileNameURLToPath.js';
+import { createGzip } from 'zlib';
+import { getDirectoryNamePath } from '../fs/utils/getDirectoryNamePath.js';
+import {createReadStream, createWriteStream} from "fs";
+const filename = fileNameURLToPath(import.meta.url);
+const dirname = getDirectoryNamePath(filename);
 const compress = async () => {
-    // Write your code here 
+    const compressFile = `${dirname}/files/fileToCompress.txt`;
+    const archiveFile = `${dirname}/files/archive.gz`;
+    const readStream = createReadStream(compressFile);
+    const writeStream = createWriteStream(archiveFile);
+    const toGz = createGzip();
+    readStream.pipe(toGz).pipe(writeStream);
+    console.log('The file is archived');
 };
 
 await compress();
