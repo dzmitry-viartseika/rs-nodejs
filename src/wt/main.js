a41962aa9be05946b66849af3c365b376475648f
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-06-18 00:56:00 +0200

    task #1 - implement worker method according to requirements - add constants of statuses

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import { fileNameURLToPath } from '../fs/utils/fileNameURLToPath.js';
+import { getDirectoryNamePath } from '../fs/utils/getDirectoryNamePath.js';
+const filename = fileNameURLToPath(import.meta.url);
+const dirname = getDirectoryNamePath(filename);
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { FULFILLED, ERROR, RESOLVED } from "./constants/promiseStatuses.js";
+
 const performCalculations = async () => {
-    // Write your code here
+    const cp = cpus();
+    const worker = `${dirname}/worker.js`;
+    let number = 10;
+    const workersResult = await Promise.allSettled(cp.map(() => {
+        return new Promise((resolve, reject) => {
+            const workerFile = `${dirname}/worker.js`;
+            const worker = new Worker(workerFile, {
+                workerData: number++
+            })
+            worker.on('message', message => resolve(message));
+            worker.on('error', message => resolve(message));
+        })
+    }));
+
+    const result = workersResult.map(({status, value}) => ({
+        status: status === FULFILLED ? RESOLVED : ERROR,
+        data: status === FULFILLED ? value: null
+    }));
+
+    console.log('result', result);
+
+    return result;
 };
 
 await performCalculations();
