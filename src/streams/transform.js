26b28395faea488efa8fd30647f0a3e49311cb87
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-06-18 00:00:44 +0200

    task #1 - implement transform method according to requirements

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'stream';
+import { EOL } from 'os'
 const transform = async () => {
-    // Write your code here 
+    const revert = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, `${chunk.toString().replace(EOL, '').split('').reverse().join('')}EOL`)
+        }
+    })
+
+    process.stdin.pipe(revert).pipe(process.stdout);
+    console.log('Write to console')
 };
 
 await transform();
